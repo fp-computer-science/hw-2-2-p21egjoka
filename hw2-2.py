e030f45be97f599c59eebdd646937298223bdee3
Emanuel Gjoka <p21egjoka@fairfieldprep.org>
2020-09-23 06:54:00 -0400

    HW2-2
    Work

---
+++ hw2-2.py
@@ -0,0 +1,14 @@
+# Author: Emanuel Mark Gjoka (AMDG) 9/22/2020
+
+print("Hello World")
+
+free_throws = 1
+two_pointers = 2
+three_pointers = 3
+
+print(free_throws)
+print(two_pointers)
+print(three_pointers)
+
+
+print("The player scored 22 points")
